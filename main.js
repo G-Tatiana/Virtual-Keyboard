445b448c3146a5c771ded325e81d5f189dd24fe2
Tatiana Gepard <gepardtatiana@gmail.com>
2023-05-01 15:46:03 +0300

    feat: create div with class row, add five clones and add style for this elements

--- main.js
+++ main.js
@@ -9,6 +9,22 @@ const wrapp = document.createElement('div');
 wrapp.classList.add('keyboard_wrapp');
 document.querySelector('main').append(wrapp);
 
-const keys = document.createElement('div');
-keys.classList.add('keyboard_keys');
-document.querySelector('div').append(keys);
+const keyboardKeys = document.createElement('div');
+keyboardKeys.classList.add('keyboard_keys');
+wrapp.append(keyboardKeys);
+
+const row = document.createElement('div');
+row.classList.add('row');
+keyboardKeys.append(row);
+
+const row2 = row.cloneNode(true);
+keyboardKeys.append(row2);
+
+const row3 = row.cloneNode(true);
+keyboardKeys.append(row3);
+
+const row4 = row.cloneNode(true);
+keyboardKeys.append(row4);
+
+const row5 = row.cloneNode(true);
+keyboardKeys.append(row5);
