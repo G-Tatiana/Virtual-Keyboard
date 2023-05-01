1f58dcb8f50f0fc073dc0a0f6c3e96adf3ba2676
Tatiana Gepard <gepardtatiana@gmail.com>
2023-05-01 14:19:05 +0300

    feat: add div with class container, add styles for this div

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 const bodyMain = document.getElementsByTagName('body')[0];
 bodyMain.classList.add('body');
+
+const main = document.createElement('main');
+main.classList.add('container');
+document.body.prepend(main);
