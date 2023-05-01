603f98474cc2941f8e5841f5565ecefd20af0268
Tatiana Gepard <gepardtatiana@gmail.com>
2023-05-01 15:09:46 +0300

    feat: add div with class keyboard_wrapp, add styles for this div

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ bodyMain.classList.add('body');
 const main = document.createElement('main');
 main.classList.add('container');
 document.body.prepend(main);
+
+const wrapp = document.createElement('div');
+wrapp.classList.add('keyboard_wrapp');
+document.querySelector('main').append(wrapp);
